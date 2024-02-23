01d9c3da3803120959c6bbe41c568415c4ef8ef0
Cicanci Stanislav <stanislavacicanci@gmail.com>
2024-02-23 10:58:38 +0200

    First commit

---
+++ database/migrations/2024_02_23_081051_create_elevi_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('elevi', function (Blueprint $table) {
+            $table->id();
+            $table->string('nume');
+            $table->string('prenume');
+            $table->string('adresa');
+            $table->string('email')->unique();
+            $table->date('data_nasterii');
+            $table->string('sex');
+            $table->double('media_bac');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('elevi');
+    }
+};
